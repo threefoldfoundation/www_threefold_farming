e2b371109a90bd5899fa7567e8a4ef2dd74c6b60
hamdy <hamdy.a.farag@gmail.com>
2020-12-10 12:11:34 +0200

    remove filter when not needed

--- src/templates/Team.vue
+++ src/templates/Team.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout>
-    <TagFilterHeader :tags="memberships" selected="all" />
+    <TagFilterHeader :tags="memberships" selected="all" v-if="$page.memberships.edges.length > 1"/>
     <div class="container sm:pxi-0 mx-auto mt-8 overflow-x-hidden">
       <div class="flex flex-wrap with-large pt-8 pb-8 mx-4 sm:-mx-4">
         <PostListItem
