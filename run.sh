9f6f4cd07d4eab198f06657dd87fa7a323300d27
kristof <kristof@incubaid.com>
2020-12-15 22:07:01 +0100

    install

--- run.sh
+++ run.sh
@@ -16,4 +16,11 @@ ln -s ~/code/github/threefoldfoundation/data_threefold/content/person content/pe
 ln -s ~/code/github/threefoldfoundation/data_threefold/content/news content/news
 ln -s ~/code/github/threefoldfoundation/data_threefold/content/project content/project
 
+export NVM_DIR="$HOME/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm use --lts
+
+
 gridsome develop
