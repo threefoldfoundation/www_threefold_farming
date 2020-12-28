333e89528e7fb9e0a73b28d54b4c88bb8908681e
ehab-hassan <hassane@codescalers.com>
2020-12-28 23:39:41 +0200

    update participate

--- src/components/marketing/sections/cta-sections/BrandPanel.vue
+++ src/components/marketing/sections/cta-sections/BrandPanel.vue
@@ -2,7 +2,7 @@
   <div class="bg-white">
     <div class="max-w-screen-xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
       <div
-        class="bg-indigo-700 rounded-lg shadow-xl overflow-hidden lg:grid lg:grid-cols-2 lg:gap-4"
+        class="bg-gray-800 rounded-lg shadow-xl overflow-hidden lg:grid lg:grid-cols-2 lg:gap-4"
       >
         <div
           class="pt-10 pb-12 px-6 sm:pt-16 sm:px-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-20"
@@ -14,14 +14,15 @@
               <span class="block">{{ brand.title }}</span>
               <span class="block">{{ brand.title2 }}</span>
             </h2>
-            <p class="mt-4 text-lg leading-6 text-indigo-200">
+            <p class="mt-4 text-lg leading-6 text-gray-200">
               {{ brand.excerpt }}
             </p>
             <g-link
               :to="brand.sourceUrl"
-              class="mt-8 bg-white border border-transparent rounded-md shadow px-6 py-3 inline-flex items-center text-base leading-6 font-medium text-indigo-600 hover:text-indigo-500 hover:bg-gray-50 transition duration-150 ease-in-out"
+              class="mt-8 bg-white shadow px-6 py-3 inline-flex items-center text-base rounded shadow leading-none font-semibold text-gray-800 hover:bg-gray-300 "
               >{{ brand.btnTxt }}</g-link
             >
+            <!-- class="inline-block py-4 px-8 mr-6 leading-none text-white bg-gray-900 hover:bg-gray-700 font-semibold rounded shadow" -->
           </div>
         </div>
         <div class="relative pb-3/5 -mt-6 md:pb-1/2">
