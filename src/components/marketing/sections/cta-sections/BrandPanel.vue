18e9a495491f73e847a8a0e1b405c22dac847e54
samaradel <samar@incubaid.com>
2020-12-30 16:22:47 +0200

    Fix brand panel in dark mode

--- src/components/marketing/sections/cta-sections/BrandPanel.vue
+++ src/components/marketing/sections/cta-sections/BrandPanel.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="bg-white">
-    <div class="max-w-screen-xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+    <div class="max-w-screen-xl mx-auto py-16">
       <div
-        class="bg-indigo-700 rounded-lg shadow-xl overflow-hidden lg:grid lg:grid-cols-2 lg:gap-4"
+        class="bg-gray-800 rounded-lg shadow-xl overflow-hidden lg:grid lg:grid-cols-2 lg:gap-4"
       >
         <div
           class="pt-10 pb-12 px-6 sm:pt-16 sm:px-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-20"
