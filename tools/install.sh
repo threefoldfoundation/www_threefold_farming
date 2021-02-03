cbba70857d9640ac29afd58ac4403e5b7426ce16
Hamdy <hamdy.a.farag@gmail.com>
2021-02-03 16:46:12 +0200

    Update install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -35,11 +35,11 @@ rm -f yarn.lock
 rm -rf node_modules
 rm -rf .cache
 
-export NVM_DIR="$HOME/nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+#export NVM_DIR="$HOME/nvm"
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-nvm use --lts
+#nvm use --lts
 
 set +e
 
