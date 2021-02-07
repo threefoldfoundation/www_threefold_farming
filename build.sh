e751ab209c42cd2de0ff96d1992f9fbc2d416b2a
kristof <kristof@incubaid.com>
2021-02-07 11:55:45 +0200

    installer cleanup

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 
 
 set -e
-cd /Users/despiegk/codesync/github/threefoldfoundation/www_threefold_farming
+cd /Users/despiegk/codewww/github/threefoldfoundation/www_threefold_farming
 
 #need to ignore errors for getting nvm not sure why
 set +e
@@ -12,5 +12,16 @@ nvm use --lts
 
 export PATH=/Users/despiegk/.publisher/versions/node/v15.8.0/bin:$PATH
 
+set +e
 gridsome build
+
+set -e
+
+mkdir -p /Users/despiegk/.publisher/publish/www_threefold_farming
+rsync -ra --delete /Users/despiegk/codewww/github/threefoldfoundation/www_threefold_farming/dist/ /Users/despiegk/.publisher/publish/www_threefold_farming/
+
+cd /Users/despiegk/codewww/github/threefoldfoundation/www_threefold_farming/dist
+
+echo go to http://localhost:9999/
+ python3 -m http.server 9999
 
