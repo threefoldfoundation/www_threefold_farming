3c2a54d5273acf6d21740c3f00038905d1b3d2f5
Vilnite <vilnitekristine@gmail.com>
2021-02-09 17:36:46 +0200

    build.sh added

--- build.sh
+++ build.sh
@@ -1,26 +1,26 @@
 
 
 set -e
-cd /Users/despiegk/codewww/github/threefoldfoundation/www_threefold_farming
+cd /Users/kristinevilnite/codewww/github/threefoldfoundation/www_threefold_farming
 
 #need to ignore errors for getting nvm not sure why
 set +e
-source /Users/despiegk/.publisher/nvm.sh
+source /Users/kristinevilnite/.publisher/nvm.sh
 
 set -e
 nvm use --lts
 
-export PATH=/Users/despiegk/.publisher/versions/node/v15.8.0/bin:$PATH
+export PATH=/Users/kristinevilnite/.publisher/versions/node/v15.8.0/bin:$PATH
 
 set +e
 gridsome build
 
 set -e
 
-mkdir -p /Users/despiegk/.publisher/publish/www_threefold_farming
-rsync -ra --delete /Users/despiegk/codewww/github/threefoldfoundation/www_threefold_farming/dist/ /Users/despiegk/.publisher/publish/www_threefold_farming/
+mkdir -p /Users/kristinevilnite/.publisher/publish/www_threefold_farming
+rsync -ra --delete /Users/kristinevilnite/codewww/github/threefoldfoundation/www_threefold_farming/dist/ /Users/kristinevilnite/.publisher/publish/www_threefold_farming/
 
-cd /Users/despiegk/codewww/github/threefoldfoundation/www_threefold_farming/dist
+cd /Users/kristinevilnite/codewww/github/threefoldfoundation/www_threefold_farming/dist
 
 #echo go to http://localhost:9999/
  #python3 -m http.server 9999
