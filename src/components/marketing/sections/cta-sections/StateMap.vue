e7d2ec1a74891fecccd4f92b0ade45b5837f6164
samaradel <samar@incubaid.com>
2021-07-15 08:44:25 +0000

    Align map

--- src/components/marketing/sections/cta-sections/StateMap.vue
+++ src/components/marketing/sections/cta-sections/StateMap.vue
@@ -1,8 +1,18 @@
 <template>
   <section class="px-2">
-    <div class="flex flex-wrap blue p-12 text-center lg:text-left -mx-2">
+    <div class="flex blue flex-wrap p-12 text-center lg:text-left -mx-2">
       <div
-        class="lg:w-1/4 light-blue text-center rounded-lg py-10 mt-10 lg:mt-0 order-1 lg:order-none"
+        class="
+          lg:w-1/4
+          light-blue
+          text-center
+          rounded-lg
+          py-10
+          mt-10
+          lg:mt-0
+          order-1
+          lg:order-none
+        "
       >
         <div class="relative max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
           <dl class="">
@@ -13,16 +23,13 @@
             >
               <div v-for="(value, key) in item" :key="key">
                 <dd
-                  class="text-5xl leading-none font-bold green-color"
+                  class="leading-none font-bold green-color"
                   :class="{ green: index % 2 !== 0 }"
                   aria-describedby="item-1"
                 >
                   {{ value }}
                 </dd>
-                <dt
-                  class="mt-2 text-lg leading-6 font-medium text-white uppercase"
-                  id="item-1"
-                >
+                <dt class="mt-2 leading-6 text-white uppercase" id="item-1">
                   {{ key }}
                 </dt>
               </div>
@@ -31,14 +38,29 @@
         </div>
       </div>
       <div
-        class="lg:w-3/4 text-center rounded lg:px-6 mt-10 lg:mt-0 order-1 lg:order-none"
+        class="
+          lg:w-3/4
+          text-center
+          rounded
+          lg:px-6
+          mt-10
+          lg:mt-0
+          order-1
+          lg:order-none
+        "
       >
         <h2
-          class="text-4xl leading-tight text-white text-center mb-12 font-normal font-heading uppercase"
+          class="
+            text-4xl text-white
+            leading-tight
+            mb-6
+            font-bold font-heading
+            uppercase
+          "
         >
           {{ section.title }}
         </h2>
-        <div class="mb-8 text-white font-light text-xl leading-8 mt-4" v-html="section.content"></div>
+        <div class="text-white mt-4" v-html="section.content"></div>
         <g-image
           class="lg:w-3/4 mx-auto"
           :src="img(section.image)"
@@ -46,14 +68,62 @@
         />
         <div class="mx-auto mt-6">
           <g-link
-            class="inline-block green text-sm learn-button hover:bg-blue-800 text-gray-100 px-12 py-2 mr-5 mb-4 rounded shadow rounded-full"
+            v-if="section.link1"
+            class="
+              inline-block
+              green
+              text-sm
+              learn-button
+              hover:bg-blue-800
+              text-gray-100
+              px-12
+              py-2
+              mr-5
+              mb-4
+              rounded
+              shadow
+              rounded-full
+            "
             :to="section.link1"
             >{{ section.btn1 }}
           </g-link>
-          <g-link
-            class="inline-block green text-sm learn-button hover:bg-blue-800 text-gray-100 px-12 py-2 mr-5 mb-4 rounded shadow rounded-full"
+          <g-link v-if="section.link2"
+            class="
+              inline-block
+              green
+              text-sm
+              learn-button
+              hover:bg-blue-800
+              text-gray-100
+              px-12
+              py-2
+              mr-5
+              mb-4
+              rounded
+              shadow
+              rounded-full
+            "
             :to="section.link2"
             >{{ section.btn2 }}</g-link
+          >
+          <g-link v-if="section.link3"
+            class="
+              inline-block
+              green
+              text-sm
+              learn-button
+              hover:bg-blue-800
+              text-gray-100
+              px-12
+              py-2
+              mr-5
+              mb-4
+              rounded
+              shadow
+              rounded-full
+            "
+            :to="section.link3"
+            >{{ section.btn3 }}</g-link
           >
           <!-- <div v-html="main.content" class="text-sm text-gray-400 mt-5"></div> -->
         </div>
@@ -88,12 +158,12 @@ export default {
         "https://explorer.threefold.io/api/stats"
       );
       let farms = getFarms.data.length;
-      let hru = (results.data.hru /1000 ).toFixed();
-      let cru = (results.data.cru ).toFixed();
+      let hru = (results.data.hru / 1000).toFixed();
+      let cru = results.data.cru.toFixed();
       this.stats.push(
         { farms: farms },
         { "Storage TB": hru },
-        { "CPU Cores": cru }
+        { "Compute Cores": cru }
       );
     } catch (error) {
       console.log(error);
@@ -112,14 +182,15 @@ export default {
 }
 
 dd {
-  height: 180px;
-  width: 180px;
+  height: 100px;
+  width: 100px;
   margin: auto;
   border-radius: 50%;
-  line-height: 180px;
+  line-height: 100px;
   border: 3px solid #847fc2;
   background-color: #313f92;
   color: #dacef5;
+  font-size: 1.5rem;
 }
 
 .green {
